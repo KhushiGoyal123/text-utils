e21229531926c2d49b66154e69655f6c727cb949
KhushiGoyal123 <khushi030201goyal@gmail.com>
2022-01-29 17:42:11 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import Alert from './components/Alert';
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+  // whether dark mode is enabled or not.
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark Mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
+      
+      // not a good user experience - title instant blink
+      setInterval(() => {
+        document.title = 'TextUtils is Fab!';
+      }, 2000);
+      setInterval(() => {
+        document.title = 'Install it Now!!';
+      }, 1500);
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled", "success");
+      document.title = 'TextUtils - Light Mode';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      {/* given prop */}
+      <Alert alert={alert}/>
+      {/* <Navbar /> */}
+      {/* default prop */}
+      <div className="container my-3">
+        <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
+      </div>
+          <About />
+      </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked"+ text);
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Text converted to UpperCase", "success");
+    }
+    const handleLowClick = () => {
+        // console.log("Lowercase was clicked"+ text);
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Text converted to LowerCase", "success");
+    }
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value);
+    }
+    
+    const [text, setText] = useState('');
+    return (
+        <>
+    <div className="container" style={{color : props.mode==='dark'?'white':'black'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor : props.mode==='light'?'#adb5bd':'white'}} id="myBox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-success mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-success mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
+    </div>
+    <div className="container my-3" style={{color : props.mode==='dark'?'white':'black'}}>
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter something in the above testbox to preview it here"}</p>
+    </div>
+    </>
+    )
+}
